4c1c60c0e3fe718183978f712c06824cba351948
Paivi Suomela <paivi.e.suomela@gmail.com>
2017-03-27 12:08:22 +0100

    started adding static files

--- motor_control/settings.py
+++ motor_control/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 # deployment
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
